a50da88a7150aac2908848dd64fed8ae61317478
Phil Engles <englep10@gmail.com>
2015-04-14 02:29:33 -0400

    Broken nested forms

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,18 @@
 class Listing < ActiveRecord::Base
 	belongs_to :user
+
 	has_many :contact_information
 	has_many :aircraft_specs
-	has_attached_file :image, styles: { :medium => "400x400#>" }, :default_url => "/images/:style/missing.png"
-  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+	accepts_nested_attributes_for :contact_information,
+  															reject_if: proc { |attributes| attributes['name'].blank? },
+  															allow_destroy: true
+ 	accepts_nested_attributes_for :aircraft_specs,
+  															reject_if: proc { |attributes| attributes['ask_price'].blank? },
+  															allow_destroy: true
+
+  validates :title, :description, :image, presence: true
+
+	has_attached_file :image, styles: { :medium => "400x400#" }
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
